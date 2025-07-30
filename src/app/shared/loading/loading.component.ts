68a82f8bd089455008871e758f3d081704dcf2f9
Merveille Jora <114751342+DevJora@users.noreply.github.com>
2025-07-30 19:09:04 +0200

    Update loading.component.ts

--- src/app/shared/loading/loading.component.ts
+++ src/app/shared/loading/loading.component.ts
@@ -1,14 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {AsyncPipe, NgIf} from '@angular/common';
 import {LoadingService} from '../../core/loading.service';
-import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-loading',
   imports: [
     NgIf,
     AsyncPipe,
-    RouterLink
   ],
   templateUrl: './loading.component.html',
   styleUrl: './loading.component.css'
